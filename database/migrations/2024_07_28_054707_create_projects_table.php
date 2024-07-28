ad6154da0d35c2cbef21c87dfdb94593c71040d5
ecasallas07 <ecasallas07@gmail.com>
2024-07-28 01:15:08 -0500

    first commit

---
+++ database/migrations/2024_07_28_054707_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id()->first();
+            $table->string('title')->nullabe()->comment();
+            $table->string('stack')->comments();
+            $table->longText('description')->comment();
+            $table->string('link_repo',2555)->comment();
+            $table->longText('activities')->comment();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <slot:
+
+
+</x-layout>
+
